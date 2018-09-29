31c8fc974837cf44985efbc8706559e5ca1d543e
TheCsWorld <43708569+TheCsWorld@users.noreply.github.com>
2018-09-29 22:16:45 +0100

    Add files via upload

---
+++ ChangeStringCase.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+/*reads a string from the keyboard, and outputs the string twice in a row, first all
+uppercase and next all lowercase. Example the string “Hello" is given, the output will be “HELLOhello"*/
+public class ChangeStringCase {
+
+	public static void main(String[] args) {
+		String word;
+		System.out.print("Enter a word: ");
+		Scanner inputScanner = new Scanner(System.in);
+		word = inputScanner.nextLine();
+		inputScanner.close();
+		System.out.print(word.toUpperCase() + " " + word.toLowerCase());		//need to put in " " so the words will be spaced
+	}
+
+}
